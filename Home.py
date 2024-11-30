439f93d3cfe4972d2144899aaf21ce8ffbde538c
remykingson <remydushimimana24@gmail.com>
2024-11-30 16:40:51 -0600

    first commit

---
+++ Home.py
@@ -0,0 +1,76 @@
+import streamlit as st
+import os
+
+st.set_page_config(page_title="RemyMadeIt | Home")
+
+
+if "step" not in st.session_state:
+      st.session_state.step = 1
+if "info" not in st.session_state:
+      st.session_state.info = {}
+
+def go_to_step1():
+      st.session_state.step = 1
+
+def go_to_step2(name):
+      st.session_state.info["name"] = name
+      st.session_state.step = 2      
+if st.session_state.step == 1:
+      st.title("Welcom To Remy MadeIt's Blog")
+      st.caption("We gland to see you here!") 
+      name = st.text_input("**Enter your Name:**")
+      submit = st.button("Submit",key="unique")
+      if submit:
+            if not name:
+                  st.write("Please fill the empty field to continue!!")
+            else:
+                  st.caption("You'are successfully submitted to our Blog! you can continue now!!")
+                  st.balloons()
+                  st.button(label="Continue", on_click=go_to_step2, args=(name,))
+       
+
+elif st.session_state.step == 2:
+      with st.container():
+            col1, col2 = st.columns(2)
+            with col1:
+                st.button("Back", on_click=go_to_step1())
+            with col2:
+                  st.subheader(f"**Welcome**, {st.session_state.info.get("name")}")
+      st.divider()
+      st.title("Our Services")
+      st.write("Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt quo, ducimus error eos excepturi illo rem voluptatum hic! Numquam autem neque similique architecto blanditiis provident ipsam tempore sunt aperiam ratione ut doloribus eaque sit nam ea reiciendis, voluptas voluptatum, officiis mollitia, quia voluptatem temporibus. Necessitatibus laudantium eligendi, facere aperiam laborum nisi dignissimos eveniet, error dolor quod repudiandae nostrum ipsa inventore corporis ea eos veniam quae nobis eius tempora provident iure reiciendis a molestiae. Nulla necessitatibus cupiditate et nisi eligendi voluptas harum, magni quia blanditiis, unde aliquam eum officia similique, accusamus sint? Nulla voluptatum expedita enim, doloribus atque exercitationem vitae ab!")
+      with st.container(border= True):
+            col1, col2 = st.columns(2)
+            with col1:
+                  st.subheader("Graphic Design")
+                  st.write("We offer you the best graphic design that enhance user  experience and effectively convey brand messages. Whether it's designing logos, crafting marketing materials, or developing intuitive UI/UX designs, I strive to deliver high-quality work that exceeds expectations.")
+            with col2:
+                  tab3, tab4, tab1, tab2 = st.tabs( {"PhotoManipulation", "PostDesign", "Logos", "Branding"})
+                  with tab1:
+                      st.image(os.path.join(os.getcwd(), "static","bro.jpg"))
+                  with tab2:
+                      st.image(os.path.join(os.getcwd(), "static","chi.jpg"))
+                  with tab3:
+                      st.image(os.path.join(os.getcwd(), "static","Logo.jpg"))
+                  with tab4:
+                      st.image(os.path.join(os.getcwd(), "static","visit.jpg"))
+      with st.container(border= True):
+            col1, col2 = st.columns(2)
+            with col1:
+                  tab1, tab2 = st.tabs( { "Designing UI/UX", "Website-Design / System-Design"})
+                  with tab1:
+                      st.write("UI/UX Designer As a UI/UX designer, I focus on creating intuitive and engaging user interfaces that provide a seamless experience. I believe that great design is not only about looks but also about how it works. I employ user-centered design principles to ensure that the interfaces I create are easy to use, efficient, and accessible. By conducting user research, creating wireframes, and iterating on designs based on feedback, I aim to build products that users love.")                      
+                  with tab2:
+                      st.write("Website Designer As a website designer, I focus on creating visually stunning and highly functional websites that cater to the needs of my clients and their users. I work on everything from layout and visual elements to user interface (UI) and user experience (UX) design. My approach is to ensure that each website not only looks great but also provides an intuitive and engaging experience. By combining modern design principles with innovative techniques, I build websites that are both aesthetically pleasing and easy to navigate.")
+                      
+            with col2:
+                  st.subheader("Website Development")
+                  st.write("Developer I specialize in building responsive, user-friendly websites and applications. My expertise includes HTML, CSS, JavaScript, and various frameworks and libraries. I'm dedicated to writing clean, efficient code and always strive to stay updated with the latest industry trends and technologies. Whether it's developing a sleek website or a complex web application, I aim to deliver solutions that are both functional and visually appealing.")
+                 
+st.markdown("""
+<div style='text-align: center; font-size: small;'>
+    © 2024 All rights reserved by Remy
+</div>
+""", unsafe_allow_html=True)
+
+
---
+++ pages/Contact-Us.py
@@ -0,0 +1,86 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+st.set_page_config(page_title="RemyMadeIt | Contact")
+st.title("Contact us")
+
+import streamlit as st
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+def send_email(subject, body, to_email):
+    from_email = "remydushimimana24@gmail.com"  # replace with your email
+    password = "eauylksllnkenjva"  # replace with your email password
+
+    # Create the email headers and body
+    msg = MIMEMultipart()
+    msg['From'] = from_email
+    msg['To'] = to_email
+    msg['Subject'] = subject
+    msg.attach(MIMEText(body, 'plain'))
+
+    try:
+        # Connect to the server and send the email
+        server = smtplib.SMTP('smtp.gmail.com', 587)  # replace with your SMTP server and port
+        server.starttls()
+        server.login(from_email, password)
+        server.sendmail(from_email, to_email, msg.as_string())
+        server.quit()
+        return True
+    except Exception as e:
+        print(f"Failed to send email: {e}")
+        return False
+
+st.subheader("Get in touch with us here")
+with st.form(key="user_form_info"):
+    with st.container():
+        col1, col2 = st.columns(2)
+        with col1:
+            first_name = st.text_input("**Enter your First_name:**")
+        with col2:
+            last_name = st.text_input("**Enter your Last_name:**")
+    email = st.text_input("**Your Email:**")
+    message = st.text_area("**Your message:**")
+    submit = st.form_submit_button(label="Submit")
+    
+    if submit:
+        if not first_name or not last_name:
+            st.write("Please fill in all the Name fields!")
+        elif not email:
+            st.write("Please fill in the Email field!")
+        elif not message:
+            st.write("Please fill in the Message field!")
+        else:
+            subject = f"New message from {first_name} {last_name}"
+            body = f"Name: {first_name} {last_name}\nEmail: {email}\nMessage: {message}"
+            to_email = "your_email@example.com"  # replace with your receiving email address
+
+            if send_email(subject, body, to_email):
+                st.success("Your message has been sent successfully!")
+                st.balloons()
+            else:
+                st.error("There was an error sending your message.")
+
+
+st.divider()
+with st.container():
+    col1, col2 = st.columns(2)
+    with col1:
+        kigali_coords = [-1.9536, 30.0636]
+        st.subheader("Map Location")
+        map_data = pd.DataFrame(
+            np.random.randn(100, 2) / [50, 50] + kigali_coords,
+            columns= ["LON", "lat"]
+        )     
+        st.map(map_data)
+    with col2:
+         st.subheader("You can find us on")
+         st.write("**Number**:  " " +250 795 057 383")
+         st.write("**Email**:  " " remydushimimana24@gmail.com")
+         st.write("**Street**: " " KN250 / Nyamirambo")
+st.markdown("""
+<div style='text-align: center; font-size: small;'>
+    © 2024 All rights reserved by Remy
+</div>
+""", unsafe_allow_html=True)         
