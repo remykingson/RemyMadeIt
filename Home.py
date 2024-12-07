ea65cc38d5d11786a4c4330a7cc7c68e9c776dca
remykingson <remydushimimana24@gmail.com>
2024-12-07 12:20:04 -0600

    first commit

--- Home.py
+++ Home.py
@@ -66,9 +66,9 @@ elif st.session_state.step == 2:
       with st.container(border= True):
             col1, col2 = st.columns(2)
             with col1:
-                  tab1, tab2 = st.tabs( { "Designing UI/UX", "Website-Design / System-Design"})
+                  tab2, tab1 = st.tabs( { "Designing UI/UX", "Website-Design / System-Design"})
                   with tab1:
-                      st.write("UI/UX Designer As a UI/UX designer, I focus on creating intuitive and engaging user interfaces that provide a seamless experience. I believe that great design is not only about looks but also about how it works. I employ user-centered design principles to ensure that the interfaces I create are easy to use, efficient, and accessible. By conducting user research, creating wireframes, and iterating on designs based on feedback, I aim to build products that users love.")                      
+                      st.write("As a UI/UX designer, I focus on creating intuitive and engaging user interfaces that provide a seamless experience. I believe that great design is not only about looks but also about how it works. I employ user-centered design principles to ensure that the interfaces I create are easy to use, efficient, and accessible. By conducting user research, creating wireframes, and iterating on designs based on feedback, I aim to build products that users love.")                      
                   with tab2:
                       st.write("Website Designer As a website designer, I focus on creating visually stunning and highly functional websites that cater to the needs of my clients and their users. I work on everything from layout and visual elements to user interface (UI) and user experience (UX) design. My approach is to ensure that each website not only looks great but also provides an intuitive and engaging experience. By combining modern design principles with innovative techniques, I build websites that are both aesthetically pleasing and easy to navigate.")
                       
